8d99ea4518a753b55161b90ed96b99037dca4e94
Jessie Yang <fenyang@thoughtworks.com>
2019-04-25 13:44:19 +0800

    [jessie] #feature-2: should park success when a is full and b is available

--- src/test/java/GraduateParkingBoyTest.java
+++ src/test/java/GraduateParkingBoyTest.java
@@ -10,11 +10,23 @@ public class GraduateParkingBoyTest {
     void should_success_and_park_in_a_parking_lot_when_park_given_a_car_with_number_and_both_a_and_b_parking_lot_is_available() throws Exception {
         ParkingLot parkingLotA = new ParkingLot(1);
         ParkingLot parkingLotB = new ParkingLot(1);
-
         GraduateParkingBoy graduateParkingBoy = new GraduateParkingBoy(Arrays.asList(parkingLotA, parkingLotB));
         Ticket ticket = graduateParkingBoy.park(new Car("A12345"));
+
         assertNotNull(ticket);
         assertEquals("A12345", parkingLotA.pick(ticket).getCarNumber());
         assertThrows(UnmatchedTicketException.class, () -> parkingLotB.pick(ticket));
+    }
+
+    @Test
+    void should_success_and_park_in_b_parking_lot_when_park_given_a_car_with_number_and_a_is_not_available_and_b_parking_lot_is_available() throws Exception {
+        ParkingLot parkingLotA = new ParkingLot(0);
+        ParkingLot parkingLotB = new ParkingLot(1);
+        GraduateParkingBoy graduateParkingBoy = new GraduateParkingBoy(Arrays.asList(parkingLotA, parkingLotB));
+        Ticket ticket = graduateParkingBoy.park(new Car("A12345"));
+
+        assertNotNull(ticket);
+        assertEquals("A12345", parkingLotB.pick(ticket).getCarNumber());
+        assertThrows(UnmatchedTicketException.class, () -> parkingLotA.pick(ticket));
     }
 }
